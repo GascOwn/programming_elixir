88c1a51e8a6a3304956211398c0185af0cf95f1c
Daniele Anselmo <74472571+GascOwn@users.noreply.github.com>
2022-09-07 22:44:30 +0200

    refactoring, lists

---
+++ lib/chapter6.ex
@@ -0,0 +1,45 @@
+defmodule Chop do
+
+  def guess(actual, range) do
+    a..b = range
+    current_guess = div(a + b, 2)
+    IO.puts "Is it #{current_guess}?"
+    guess_helper(actual, range, current_guess)
+  end
+
+  def guess_helper(actual, _ , actual), do: IO.puts actual
+  def guess_helper(actual, a.._, guess) when guess > actual do
+    guess(actual, a..(guess - 1))
+  end
+  def guess_helper(actual, _..b, guess) when guess < actual do
+    guess(actual, (guess + 1)..b)
+  end
+
+end
+
+defmodule PipeMath do
+  def pipe_range(range) do
+    range |> Enum.map(&(&1 * 2)) |> Enum.filter(&(&1 <= 30))
+  end
+end
+
+defmodule Example do
+#  def compile_and_go(source) do
+#    alias My.Other.Module.Parser, as: Parser
+#    alias My.Other.Module.Runner, as: Runner
+#    source
+#    |> Parser.parse()
+#    |> Runner.execute()
+#  end
+  @author "Dave Thomas"
+  def func1 do
+    List.flatten [1,[2,3],4]
+  end
+  def func2 do
+    import List, only: [flatten: 1]
+    flatten [5,[6,7],8]
+  end
+  def get_author do
+    @author
+  end
+end
---
+++ lib/chapter7.ex
@@ -0,0 +1,24 @@
+defmodule MyList do
+  def len([]), do: 0
+  def len([ _head | tail ]), do: 1 + len(tail)
+
+  def square([]), do: []
+  def square([head | tail]), do: [head * head | square(tail)]
+
+  def add_1([]), do: []
+  def add_1([ head | tail ]), do: [ head + 1 | add_1(tail) ]
+
+  def mymap([], _), do: []
+  def mymap([head | tail], func), do: [func.(head) | mymap(tail, func)]
+
+  def reduce([], value, _), do: value
+  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
+
+  def mapsum([], _), do: 0
+  def mapsum([head | tail], func) when is_integer(head) do
+    func.(head) + mapsum(tail, func)
+  end
+  def bigger(a, b) when a > b, do: a
+  def bigger(a, b) when a <= b, do: b
+  def max([head | tail]), do: reduce(tail, head, &bigger/2)
+end
